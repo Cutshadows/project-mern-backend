fc2399471e2c1456874982572fbbdff15d1ba673
Cutshadows <ramnstein_1749_@hotmail.com>
2020-08-07 21:59:45 -0400

    user validation

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,10 @@ const auth=require('../middleware/authentication');
 
 //api/auth
 router.post('/',
-[
-    check('email', 'Agrega un email valido') .isEmail(),
-    check('password', 'El password debe ser minimo de 6 caracteres').isLength({min:6})
-],
+/*[
+    //check('email', 'Agrega un email valido') .isEmail(),
+    //check('password', 'El password debe ser minimo de 6 caracteres').isLength({min:6})
+],*/
     authServices.authUser
 );
 
